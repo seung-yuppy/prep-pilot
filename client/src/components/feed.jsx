309fe19412e1707a19088985cd2e5c03656eb7ce
seung-yuppy <songsy0107@gmail.com>
2025-07-25 00:26:51 +0900

    Fix. SCSS 파일 스타일 수정 및 다크/라이트모드 메인만 구현

--- client/src/components/feed.jsx
+++ client/src/components/feed.jsx
@@ -1,33 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function Feed({ id, content, title, createdAt }) {
   return (
     <>
-     <a href={`/post/${id}`}>
-      <div className="feed-card">
-        <img
-          src="https://velog.velcdn.com/images/takuya/post/d3645507-ad33-4aff-b8fd-d51f30bd481e/image.png"
-          alt=""
-          className="feed-thumnail"
-        />
-        <div className="feed-content">
-          <h1 className="feed-title">{title}</h1>
-          <p className="feed-description">
-            {content}
-          </p>
-          <div className="feed-info">
-            <span className="info-date">{createdAt}</span>
-            <span className="info-dot">•</span>
-            <span className="info-comment">3개의 댓글</span>
-          </div>
-          <div className="feed-footer">
-            <div className="footer-nickname">
-              <span className="nickname-by">By</span>
-              <a href="#"><span className="nickname-name">배고픈 둘리</span></a>
+      <Link to={`/post/${id}`}>
+        <div className="feed-card">
+          <img
+            src="https://velog.velcdn.com/images/takuya/post/d3645507-ad33-4aff-b8fd-d51f30bd481e/image.png"
+            alt=""
+            className="feed-thumnail"
+          />
+          <div className="feed-content">
+            <h1 className="feed-title">{title}</h1>
+            <p className="feed-description">{content}</p>
+            <div className="feed-info">
+              <span className="info-date">{createdAt}</span>
+              <span className="info-dot">•</span>
+              <span className="info-comment">3개의 댓글</span>
             </div>
-            <span className="footer-like">♥ 125</span>
+            <div className="feed-footer">
+              <div className="footer-nickname">
+                <span className="nickname-by">By</span>
+                <span className="nickname-name">배고픈 둘리</span>
+              </div>
+              <span className="footer-like">♥ 125</span>
+            </div>
           </div>
         </div>
-      </div>
-      </a>
+      </Link>
     </>
   );
 }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -4,22 +4,34 @@ import useGetPosts from "../service/post/useGetPosts";
 
 export default function Home() {
   // const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  const { data: posts } = useGetPosts(); 
+  const { data: posts } = useGetPosts();
 
   return (
     <>
-      <div className="home-container">
-        <h1 className="home-title">✨Prep Pilot</h1>
-        <Menubar />
-      </div>
+      <div className="home-wrapper">
+        <div className="home-container">
+          <h1 className="home-title">✨Prep Pilot</h1>
+          <Menubar />
+        </div>
 
-      <div className="feed-container">
-        {/* {arr.map((_, index) => (
+        <div className="feed-container">
+          {/* {arr.map((_, index) => (
           <Feed key={index} />
         ))}  */}
-        {posts ? (posts?.content.map((value, index) => (
-          <Feed key={index} id={value?.id} title={value?.title} content={value?.content} createdAt={value?.createdAt} />
-        ))) : <h1 className="feed-nocontent">글 목록이 없습니다.</h1>} 
+          {posts ? (
+            posts?.content.map((value, index) => (
+              <Feed
+                key={index}
+                id={value?.id}
+                title={value?.title}
+                content={value?.content}
+                createdAt={value?.createdAt}
+              />
+            ))
+          ) : (
+            <h1 className="feed-nocontent">글 목록이 없습니다.</h1>
+          )}
+        </div>
       </div>
     </>
   );
