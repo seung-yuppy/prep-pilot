313e0228f959d56efcfe3255245d0bfac1b309b3
seung-yuppy <songsy0107@gmail.com>
2025-07-30 16:26:33 +0900

    Fix. 로그인 에러처리

--- client/src/util/user/login.js
+++ client/src/util/user/login.js
@@ -10,14 +10,25 @@ const onLogIn = async ({ username, password}) => {
         body: JSON.stringify({ username, password }),
         credentials: "include",
       });
+
+      if (!res.ok) {
+        if (res.status === 401) {
+          throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+        }
+        throw new Error("로그인 요청에 실패했습니다.");
+      }
       
       const accessToken = res.headers.get("access");
       if (accessToken) {
         localStorage.setItem("access", accessToken);
       }
+      if (!accessToken) {
+        throw new Error("access 토큰이 존재하지 않습니다. 로그인 실패");
+      }    
       return accessToken;
   } catch (error) {
     console.error("로그인 에러 발생", error);
+    throw error;
   }
 }
 
