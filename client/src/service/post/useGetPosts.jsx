b8e8bb4ccf10d22928e79672251ab45f73d2c422
seung-yuppy <songsy0107@gmail.com>
2025-08-04 22:43:26 +0900

    Feat. home page - infinite scroll & feed title, content text modify

--- client/src/service/post/useGetPosts.jsx
+++ client/src/service/post/useGetPosts.jsx
@@ -3,8 +3,10 @@ import getPosts from "../../util/post/getPosts";
 
 const useGetPosts = (page = 0) => {
   return useQuery({
-    queryKey: ['posts', page],
+    queryKey: ["posts", page],
     queryFn: () => getPosts(page),
+    keepPreviousData: true, // 데이터 누적 중 이전 데이터 유지, 네트워크 요청 없이 캐시 사용
+    staleTime: 1000 * 60,
   });
 };
 
